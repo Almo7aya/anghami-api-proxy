045c29605a1658be20d8ca5ed5cbdb64104eeb5f
Ali Almohaya <alialmohaya@gmail.com>
2019-09-10 08:31:56 +0300

    Add sss cookie

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const anghamiAPIsEndpoint = 'https://api.anghami.com';
 app.use(cors());
 
 app.use('/', proxy({
-    target: anghamiAPIsEndpoint,
-    changeOrigin: true,
+	target: anghamiAPIsEndpoint,
+	changeOrigin: true,
 	onProxyReq: (proxyReq, req, res) => {
 		proxyReq.setHeader('origin', 'https://widget.anghami.com');
 		proxyReq.setHeader('referer', 'https://widget.anghami.com');
+		proxyReq.setHeader('cookie', 'sss=ns55495274_fc2daa7ffa0d46af8574d4b1834f5bc4'); // fake account
 	},
 	onProxyRes: (proxyRes) => {
 		proxyRes.headers['Access-Control-Allow-Origin'] = '*';
