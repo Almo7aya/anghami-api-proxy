327e2aa8133c21c9aa64ca2824ebe90847f5fecb
Ali Almohaya <alialmohaya@gmail.com>
2019-09-16 13:31:54 +0300

    Fixed

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ app.use('/', proxy({
 	target: anghamiAPIsEndpoint,
 	changeOrigin: true,
 	onProxyReq: (proxyReq, req, res) => {
+		req.originalHost = req.headers.host
 		proxyReq.setHeader('origin', 'https://widget.anghami.com');
 		proxyReq.setHeader('referer', 'https://widget.anghami.com');
 		proxyReq.setHeader('cookie', 'sss=ns55495274_fc2daa7ffa0d46af8574d4b1834f5bc4;'); // fake account
 	},
 	onProxyRes: (proxyRes, req) => {
-		proxyRes.headers['Access-Control-Allow-Origin'] = req.headers.host;
+		proxyRes.headers['Access-Control-Allow-Origin'] = req.originalHost;
 	}
 }));
 
